06a4341b51cfa71b2ab9e782eb78981a493f17d9
João Pedro <joaobattezini@gmail.com>
2022-08-03 21:35:40 -0300

    criado as pages header e footer

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,44 @@
 const express = require('express');
 const router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+const pageController = require('../controllers/pageController')
+const searchController = require('../controllers/searchController')
 
-// GET information page
-router.get('/information', (req, res)) => {
-  res.render()
-}
 
-module.exports = router;
+router.get('/', pageController.home)
+router.get('/carrinho', pageController.carrinho)
+router.get('/information', pageController.information)
+router.get('/departament', pageController.departament)
+router.get('/cadastro', pageController.cadastro)
+router.get('/account', pageController.account)
+router.get('/login', pageController.login)
+router.get('/produtos', pageController.produtos)
+
+
+router.get('/search', searchController.search)
+
+module.exports = router 
+
+
+
+
+// EXEMPLO DE ROTAS
+
+
+// /* GET home page. */
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
+
+// // GET carrinho page
+
+// router
+
+
+// // GET information page
+// router.get('/information', (req, res)) => {
+//   res.render()
+// }
+
+// module.exports = router;
+
