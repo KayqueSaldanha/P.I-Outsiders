23f724f05fbea636dc178e2625a303f496386cf5
Leandro Furtado <le_furtado@outlook.com.br>
2022-08-20 22:46:39 -0300

    importando rotas de edição de produtos

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ const indexRouter = require('./src/routes/index');
 
 const app = express();
 
+//importando as rotas de edição de produtos
+const produtoRoutes =  require('./src/routes/produtos')
+
 // view engine setup
 app.set('views', path.join(__dirname, 'src/views'));
 app.set('view engine', 'ejs');
@@ -19,6 +22,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/adm', produtoRoutes);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
