82be8d15d539c58e54fa5d8ed3e6c7b804ae1846
Leandro Furtado <le_furtado@outlook.com.br>
2022-08-25 19:56:12 -0300

    criado rota de usuario

---
+++ src/routes/userRoute.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = express.Router();
+
+const userController = require('../controllers/userController');
+
+//formulario de registro
+router.get('/cadastro', userController.cadastro);
+
+//formulário de login
+router.get('/login', userController.login);
+
+//sessão logada
+router.get('/account', userController.account);
+
+//edição de dados do usuario
+router.get('/account_edit', userController.accountEdit);
+
+//visualização dos dados de compra
+router.get('/account_request', userController.accountRequest);
+
+module.exports = router;
