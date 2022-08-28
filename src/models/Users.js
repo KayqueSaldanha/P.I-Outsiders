e625c25b2fb370301396655e48104f5b1c558b43
Leandro Furtado <le_furtado@outlook.com.br>
2022-08-28 19:24:29 -0300

    criado funções que fazem a busca no db comparando a senha e o email fornecido no corpo da requisição

--- src/models/Users.js
+++ src/models/Users.js
@@ -1,15 +1,28 @@
 const fs = require('fs');
 const {v4} = require('uuid');
 
-let db = require('../database/db.json');
+const db = require('../database/db.json');
 
+// Escreve os dados em memória no arquivo db.json
+// Essa é uma função que é chamada quando queremos escrever dados no arquivo JSON
 const writeToDB = () => {
+    // Converte o objeto db em string
     const json = JSON.stringify(db);
+    // Escreve a string no arquivo db.json
     fs.writeFileSync('src/database/db.json', json);
-};
+}
 
 const User = {
+    // Retorna um usuário por um campo
     findAll: () => db.users,
+
+    // Retorna um usuário pelo email
+    findByEmail: (email) => {
+        // Busca o usuário que tiver o email informado
+        const user = db.users.find(user => user.email === email);
+        // Retorna o usuário encontrado
+        return user;
+    },
 
     findById: (id) => {
         const user = db.users.find(user => user.id === id);
@@ -17,12 +30,10 @@ const User = {
     },
 
     create: (user) => {
+        // Gera um id único para o usuário e adiciona ao db em memória
         db.users.push({ id: v4(), ...user });
+        // Escreve os dados em memória no arquivo db.json
         writeToDB();
-    },
-
-    account: () => {
-        
     }
 }
 
