fba7c05aca8c783a1b7a6c32ec8e87fc84951f0f
Leandro Furtado <le_furtado@outlook.com.br>
2022-08-30 15:39:51 -0300

    importando o middleware para usar em todas as rotas privadas

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const session = require('express-session');
+const userIsAuthenticated = require('./src/middlewares/userIsAuthenticated');
 
 const indexRouter = require('./src/routes/index');
 const publicRoute = require('./src/routes/publicRoute');
@@ -28,6 +29,9 @@ app.use(session({
 
 app.use('/', indexRouter);
 app.use('/users', publicRoute);
+
+app.use(userIsAuthenticated);
+
 app.use('/arealogada', privateRoute);
 
 // catch 404 and forward to error handler
