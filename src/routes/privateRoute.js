1619f922f9c6064076ffdab69c79244dee7a856c
Andressa Paranhos <andressaparanhos1@hotmail.com>
2022-09-01 22:01:08 -0300

    add: criado rota de edição

--- src/routes/privateRoute.js
+++ src/routes/privateRoute.js
@@ -13,6 +13,8 @@ const router = express.Router();
 router.get('/account', AuthController.renderAreaRestrita);
 // Rota para mostrar formulário de edição de usuário
 router.get('/edit/:id', AuthController.editForm);
+// Rota para atualizar um usuário
+router.put('/users', AuthController.edit);
 router.post('/logout', AuthController.logout);
 
 module.exports = router;
