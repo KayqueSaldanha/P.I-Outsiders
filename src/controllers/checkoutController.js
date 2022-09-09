e9406bfc98bead7a41538adfe57a9eab58c487fb
Leandro Furtado <le_furtado@outlook.com.br>
2022-09-08 23:21:40 -0300

    criado metodo add frete que adiciona o metodo de frete no JSON

--- src/controllers/checkoutController.js
+++ src/controllers/checkoutController.js
@@ -19,13 +19,34 @@ const CheckoutController = {
         // Recebe o id do usuario logado caso estiver
         const { id } = req.session.user;
         // Salva o endereço com as informações salvas nas constantes anteriores
-        Checkout.addInfo({ userid: id, endereco });
+        const idEndereco = Checkout.addEndereco({ userid: id, endereco });
         // Redireciona para a proxima etapa frete
+        req.session.endereco = idEndereco;
         res.redirect('/frete');
     },
 
     frete: (req, res) => {
         res.render('shipping');
+    },
+
+    addFrete: (req, res) => {
+        // Salvando o corpo da requisição numa constante
+        const frete = req.body;
+        // Salvando o a sessão do carrinho numa constante
+        const produtos = req.session.carrinho;
+        // Salvando o id do usuario numa constante
+        const { id } = req.session.user;
+        // Salvando o id do endereço numa constante
+        const idEndereco = req.session.endereco;
+        // Executando a função de addFrete e armazenando o retorno dela numa constante
+        const idCompra = Checkout.addFrete(id, idEndereco, produtos, frete);
+        // deixando o idCompra disponivel para ser usado em outra parte do código
+        req.session.idCompra = idCompra;
+        res.redirect('/metodo-de-pagamento');
+    },
+
+    metodoDePagamento: (req, res) => {
+        res.render('payment');
     }
 }
 
