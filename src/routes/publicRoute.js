500d3a9aee0bd46ca43a93e2e1f4804abe677736
Leandro Furtado <le_furtado@outlook.com.br>
2022-09-26 19:37:47 -0300

    comentada algumas rotas e separado apenas a rota utilizada pra adição via banco de dados

--- src/routes/publicRoute.js
+++ src/routes/publicRoute.js
@@ -5,6 +5,8 @@ const UserController = require('../controllers/UserController');
 // Importando o controller de autenticação
 const AuthController = require('../controllers/AuthController');
 
+const UsuarioController = require('../controllers/UsuarioController');
+
 const router = express.Router();
 
 // Rotas públicas para usuários não logados
@@ -15,12 +17,14 @@ router.get('/login', AuthController.renderLogin);
 
 // Renderiza a página de cadastro de usuário
 // Utiliza o middleware redirectAuthenticatedUser para redirecionar o usuário logado
-router.get('/cadastro', UserController.renderFormCadastro);
+// router.get('/cadastro', UserController.renderFormCadastro);
+// router.get('/cadastro', UsuarioController.create);
 
 // Rota para fazer o login do usuário
 router.post('/login', AuthController.login);
 
 // Rota para cadastrar um novo usuário
-router.post('/cadastro', UserController.cadastro);
+// router.post('/cadastro', UserController.cadastro);
+// router.post('/cadastro', UsuarioController.store);
 
 module.exports = router;
