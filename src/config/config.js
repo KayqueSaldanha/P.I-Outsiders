7d42d82d11033e72e4242331efcf462255c67330
Leandro Furtado <le_furtado@outlook.com.br>
2022-09-26 21:47:18 -0300

    usuario modificado

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,6 @@
 const config = {
-    username: 'Leandro',
-    password: '',
+    username: 'deploy',
+    password: 'Teste*123',
     database: 'mydb',
     host: 'localhost',
     dialect: 'mysql'
