548bb8379249308c17b28bf761146fb00656828d
Leandro Furtado <le_furtado@outlook.com.br>
2022-09-27 22:46:05 -0300

    model criada para tratar o usuário

---
+++ src/models/User.js
@@ -0,0 +1,39 @@
+module.exports = (sequelize, DataType) => {
+    const User = sequelize.define('Usuario', {
+        id: {
+            type:DataType.INTEGER,
+            primaryKey:true,
+            autoIncrement:true
+        },
+        nome: {
+            type:DataType.STRING,
+            allowNull:false
+        },
+        sobrenome:{
+            type:DataType.STRING,
+            allowNull:false
+        },
+        email: {
+            type:DataType.STRING,
+            allowNull:false
+        },
+        senha: {
+            type:DataType.STRING,
+            allowNull:false
+        },
+        cpf: {
+            type:DataType.STRING
+        },
+        telefone: {
+            type:DataType.STRING
+        },
+        dataNascimento: {
+            type:DataType.DATE
+        }
+    },{
+        tableName:'usuarios',
+        timestamps:false
+    })
+
+    return User;
+}
