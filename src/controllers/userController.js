8bec5714ce3cd1baf57100e6edc76dabf0522059
Leandro Furtado <le_furtado@outlook.com.br>
2022-09-27 22:44:56 -0300

    controller responsavel pela criação de um usuário no banco de dados com Sequelize

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,28 +1,19 @@
-const User = require('../models/Users');
+// ============================================================
+// Controller responsável pelo cadastro de um usuário no banco
+// ============================================================
+const { User } = require('../models');
 const { createMenuObject } = require('../../helpers/createMenuObject');
 const bcrypt = require('bcrypt');
 
 const saltRounds = 10;
 
 const UserController = {
-
-    cadastro: (req, res) => {
-        const { nome, sobrenome, email, password } = req.body;
-
-        // Faz a criptografia da senha
-        const hash = bcrypt.hashSync(password, saltRounds);
-
-        // Chama a model para criar um novo usuário
-        // Passando o email e a senha criptografada
-        User.create({ nome, sobrenome, email, password: hash });
-
-        // Redireciona para a página de login
-        res.redirect('/login');
+    index: async (req, res) => {
+        let usuarios = await User.findAll();
     },
+    // metodo para renderizar a página de criação de um novo usuário
+    renderFormRegister: (req, res) => {
 
-    renderFormCadastro: (req, res) => {
-        // Verifica se o usuário está logado
-        // Ou seja, se existe uma sessão para o usuário
         if (req.session.user != undefined) {
             // Se estiver logado, redireciona para a página restrita
             return res.redirect('/account');
@@ -30,7 +21,24 @@ const UserController = {
 
         // Renderiza a página de cadastro de usuário
         return res.render('cadastro', {menu: createMenuObject('false')});
-    }    
+    },
+    // método para fazer a gravação no banco de dados dos usuários
+    createNewUser: async (req, res) => {
+        // essa variável armazena os dados recebidos via body
+        const { nome, sobrenome, email, senha } = req.body;
+
+        const hash = bcrypt.hashSync(senha, saltRounds);
+
+        // esse resultado é assincrono e faz a inclusão no DB dos parametros armazenados anteriormente
+        await User.create({
+            nome,
+            sobrenome,
+            email,
+            senha: hash
+        })
+
+        return res.redirect('/');
+    }
 }
 
 module.exports = UserController;
