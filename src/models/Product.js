93dea1a8838076c99d8446045a8ff5d77c730597
Andressa Paranhos <andressaparanhos1@hotmail.com>
2022-09-27 23:52:56 -0300

    sequelize: configuração da coluna produtos

---
+++ src/models/Product.js
@@ -0,0 +1,62 @@
+/* DataType: parametros necessário p/ converter o sql em código*/
+module.exports = (sequelize, DataType) => {
+    //const Produto responsável por retornar o nome da model
+    const Product = sequelize.define('Product',{
+        //listando abaixo a representação das colunas
+        id:{
+            type:DataType.INTEGER, 
+            primaryKey: true, 
+            autoIncrement: true
+        },
+        imagem1: {
+            type:DataType.STRING,
+            allowNull:false
+        },
+        imagem2: {
+            type:DataType.STRING,
+            allowNull:true
+        },
+        imagem3: {
+            type:DataType.STRING,
+            allowNull:true
+        },
+        nome: {
+            type:DataType.STRING,
+            allowNull:false
+        },
+        preco: {
+            type:DataType.INT,
+            allowNull:false
+        },
+        cores: {
+            type:DataType.JSON,
+            allowNull:false
+        },
+        genero: {
+            type:DataType.STRING,
+            allowNull:false
+        },
+        categoria: {
+            type:DataType.STRING,
+            allowNull:false
+        },
+        tamanhos: {
+            type:DataType.JSON,
+            allowNull:false
+        },
+        status: {
+            type:DataType.STRING,
+            allowNull:true
+        },
+        descricao: {
+            type:DataType.STRING,
+            allowNull:true
+        },
+    },{
+    //nome da tabela criada no banco
+    tableName:'produtos',
+    timestamps:false
+    })
+
+    return Product;
+};
