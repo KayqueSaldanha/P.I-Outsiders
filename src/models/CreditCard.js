cfffde711ee643b4141c507e29504da5ee5fd477
Leandro Furtado <le_furtado@outlook.com.br>
2022-09-30 14:41:06 -0300

    model de cartão de credito criada

---
+++ src/models/CreditCard.js
@@ -0,0 +1,39 @@
+module.exports = (sequelize, DataType) => {
+    const CreditCard = sequelize.define('CreditCard', {
+        id: {
+            type: DataType.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        numero: {
+            type: DataType.STRING,
+            allowNull: false
+        },
+        validade: {
+            type: DataType.STRING,
+            allowNull: false
+        },
+        cvv: {
+            type: DataType.STRING,
+            allowNull: false
+        },
+        nomeImpresso: {
+            type: DataType.STRING,
+            allowNull: false
+        },
+        cpf: {
+            type: DataType.STRING,
+            allowNull: false
+        },
+        usuarioId: {
+            type:DataType.INTEGER,
+            primaryKey:true,
+            autoIncrement:true
+        }
+    }, {
+        tableName: 'cartoes',
+        timestamps: false
+    })
+
+    return CreditCard;
+}
