c4413f716fd9f6f3532c9d8f8b1d30839fed8b29
Leandro Furtado <le_furtado@outlook.com.br>
2022-09-30 14:40:49 -0300

    identação

--- src/models/Address.js
+++ src/models/Address.js
@@ -1,44 +1,46 @@
 module.exports = (sequelize, DataType) => {
     const Address = sequelize.define('Address', {
         id: {
-            type:DataType.INTEGER,
-            primaryKey:true,
-            autoIncrement:true
+            type: DataType.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
         },
         cep: {
-            type:DataType.STRING,
-            allowNull:false
+            type: DataType.STRING,
+            allowNull: false
         },
-        cidade:{
-            type:DataType.STRING,
-            allowNull:false
+        cidade: {
+            type: DataType.STRING,
+            allowNull: false
         },
         rua: {
-            type:DataType.STRING,
-            allowNull:false
+            type: DataType.STRING,
+            allowNull: false
         },
         numero: {
-            type:DataType.STRING,
-            allowNull:false
+            type: DataType.STRING,
+            allowNull: false
         },
         bairro: {
-            type:DataType.STRING,
-            allowNull:false
+            type: DataType.STRING,
+            allowNull: false
         },
         complemento: {
-            type:DataType.STRING,
-            allowNull:false
+            type: DataType.STRING,
+            allowNull: false
         },
         estado: {
-            type:DataType.STRING,
-            allowNull:false
+            type: DataType.STRING,
+            allowNull: false
         },
         usuarioId: {
-            type:DataType.INTEGER
+            type: DataType.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
         }
-    },{
-        tableName:'enderecos',
-        timestamps:false
+    }, {
+        tableName: 'enderecos',
+        timestamps: false
     })
 
     return Address;
