ba6a97c6140845ba55b7fd8fc99ce5ff982ce989
Leandro Furtado <le_furtado@outlook.com.br>
2022-10-01 13:50:24 -0300

    rotas atualizadas para os metodos correspondentes

--- src/routes/checkout.js
+++ src/routes/checkout.js
@@ -2,19 +2,22 @@ const express = require('express');
 const router = express.Router();
 
 const CheckoutController = require('../controllers/checkoutController');
+const renderShippingMethod = require('../middlewares/renderShippingMethod');
 
 // Verifica se o usuário está logado
 router.get('/information', CheckoutController.userIsNotLoggedInAddress);
 // Pega a requisição e armazena no banco de dados via post
 router.post('/information', CheckoutController.addAddress);
 // Renderiza a pagina de frete
-router.get('/frete', CheckoutController.userIsNotLoggedInShipping);
+router.get('/frete', renderShippingMethod, CheckoutController.userIsNotLoggedInShipping);
 // Adiciona o frete por post
-router.post('/frete', CheckoutController.addFrete);
+router.post('/frete', CheckoutController.addShippingMethod);
 // renderiza a pagina de pagamento
-router.get('/metodo-de-pagamento', CheckoutController.metodoDePagamentoLogado);
+router.get('/metodo-de-pagamento', CheckoutController.userIdNotLoggedInPaymentMethod);
 
-router.post('/metodo-de-pagamento', CheckoutController.metodoCartao);
+router.post('/metodo-de-pagamento', CheckoutController.addPaymentMethod);
+
+router.get('/compra-confirmada', CheckoutController.purchaseFinalization);
 
 
 
