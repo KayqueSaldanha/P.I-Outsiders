5dd7c6e386becd03cb9a9238cda5bf794e39b33e
Leandro Furtado <le_furtado@outlook.com.br>
2022-09-30 22:57:51 -0300

    corrigido o auto increment que pode ser só de um campo

--- src/models/CreditCard.js
+++ src/models/CreditCard.js
@@ -27,8 +27,7 @@ module.exports = (sequelize, DataType) => {
         },
         usuarioId: {
             type:DataType.INTEGER,
-            primaryKey:true,
-            autoIncrement:true
+            primaryKey:true
         }
     }, {
         tableName: 'cartoes',
