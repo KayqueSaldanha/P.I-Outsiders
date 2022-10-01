6b00a85336e87d0c6e4893a15bfd68f2259e2734
Leandro Furtado <le_furtado@outlook.com.br>
2022-10-01 13:48:57 -0300

    metodos de seleção de frete e metodo de pagamento adicionados

--- src/controllers/checkoutController.js
+++ src/controllers/checkoutController.js
@@ -1,24 +1,24 @@
 const User = require('../modelsJson/Users');
 const { Address } = require('../models');
+const { Shipping } = require('../models');
+const { CreditCard } = require('../models');
+const { Pix } = require('../models');
+const { Ticket } = require('../models');
+const { Purchase } = require('../models');
 const Checkout = require('../modelsJson/Checkout');
 
 const CheckoutController = {
+
     index: async (req, res) => {
         let address = await Address.findAll();
+
     },
 
     userIsNotLoggedInAddress: (req, res) => {
-        if (req.session.carrinho == undefined) {
-            return res.redirect('/');
-        }
-        res.render('information');
-        // Verifica se o usuário está logado
-        // Ou seja, se existe uma sessão para o usuário
-        if (req.session.user == undefined) {
-            // Se não estiver logado, redireciona para a página login
-            return res.redirect('/login');
-        }
-        res.render('information');
+
+        !req.session.carrinho ? res.redirect('/') : res.render('information');
+        !req.session.user ? res.redirect('/login') : res.render('information');
+        
     },
 
     addAddress: async (req, res) => {
@@ -37,80 +37,134 @@ const CheckoutController = {
                 complemento: addressInformation.complemento,
                 estado: addressInformation.estado
             }
-        })
+        });
 
         if (currentAddress) {
+
             req.session.addressId = currentAddress.id;
+
         } else if (!currentAddress) {
+
             const newAddress = await Address.create({
                 ...addressInformation,
                 usuarioId: id
             })
+
             req.session.addressId = newAddress.id;
+
         }
+
         res.redirect('/frete');
 
     },
 
     userIsNotLoggedInShipping: (req, res) => {
-        if (req.session.endereco == undefined) {
-            return res.redirect('/login');
-        }
-        res.render('shipping');
-        if (req.session.carrinho == undefined) {
-            return res.redirect('/');
-        }
-        res.render('shipping');
-        if (req.session.user == undefined) {
-            // Se não estiver logado, redireciona para a página login
-            return res.redirect('/login');
-        }
-        res.render('shipping');
+
+        !req.session.addressId ? res.redirect('/login') : res.render('shipping');
+        !req.session.carrinho ? res.redirect('/') : res.render('shipping');
+        !req.session.user ? res.redirect('/login') : res.render('shipping');
+
     },
 
-    addFrete: (req, res) => {
-        // Armazena o id do usuario da sessão numa constante chamada idUsuario
-        const idUsuario = req.session.user.id
-        // Pega o id do endereço e armazena numa constante
-        const idEndereco = req.session.endereco
-        // Pega o produto do carrinho e armazena numa constante
-        const produto = req.session.carrinho
-        // Pega a requisição do corpo e armazena na constante body
-        const body = req.body.frete;
-        // Trata a requisição do corpo que vem como texto simples e da um parse pro JSON
-        const frete = JSON.parse(body);
+    addShippingMethod: async (req, res) => {
+        // Pega a requisição do corpo e armazena na constante
+        const shippingMethodSelector = req.body.frete;
         // Executa o metodo da Model pra adicionar o frete ao JSON com os parametros especificados
-        const idCompra = Checkout.addFrete(idUsuario, idEndereco, produto, frete);
-        req.session.idCompra = idCompra;
-        console.log('aqui', idCompra);
+        const shippingId = await Shipping.findOne({
+            where: {
+                id: shippingMethodSelector
+            }
+        });
+        req.session.shippingId = shippingId.id;
         res.redirect('/metodo-de-pagamento');
     },
 
-    metodoDePagamentoLogado: (req, res) => {
-        console.log('METODO DE PAGAMENTO LOGADO');
-        if (req.session.idCompra == undefined) {
-            return res.redirect('/login');
-        }
-        res.render('payment');
-        if (req.session.carrinho == undefined) {
-            return res.redirect('/');
-        }
-        res.render('payment');
-        if (req.session.user == undefined) {
-            // Se não estiver logado, redireciona para a página login
-            return res.redirect('/login');
-        }
-        res.render('payment');
+    userIdNotLoggedInPaymentMethod: (req, res) => {
+
+        !req.session.shippingId ? res.redirect('/login') : res.render('payment');
+        !req.session.carrinho ? res.redirect('/') : res.render('payment');
+        !req.session.user ? res.redirect('/login') : res.render('payment');
+
     },
 
-    metodoCartao: (req, res) => {
-        const idCompra = req.session.idCompra;
-        const idUsuario = req.session.user.id;
-        const { metodoDePagamento, ...dadosDoCartao } = req.body;
-        console.log('ola', dadosDoCartao);
-        const idCard = Checkout.formaDePagamentoCartao(idUsuario, dadosDoCartao);
-        Checkout.atualizaCompra(idCompra, idCard, metodoDePagamento);
-        console.log("ID CARD AQUI", idCard);
+    addPaymentMethod: async (req, res) => {
+
+        const { id } = req.session.user
+        const paymentMethodSelected = req.body
+
+        if (paymentMethodSelected.metodoDePagamento === "cartao") {
+
+            const creditCardInformations = req.body;
+            const saveCard = req.body.saveCard;
+
+            if (saveCard) {
+
+                const newCreditCardSaved = await CreditCard.create({
+                    ...creditCardInformations,
+                    usuarioId: id
+                });
+
+                req.session.creditCardId = newCreditCardSaved.id;
+
+            } else {
+
+                const oldCreditCard = await CreditCard.findOne({
+                    where: {
+                        numero: creditCardInformations.numero,
+                        validade: creditCardInformations.validade,
+                        cvv: creditCardInformations.cvv,
+                        nomeImpresso: creditCardInformations.nomeImpresso,
+                        cpf: creditCardInformations.cpf
+                    }
+                });
+
+                req.session.creditCardId = oldCreditCard.id;
+                req.session.parcelNumbers = creditCardInformations.numeroParcela;
+
+            };
+
+        } else if (paymentMethodSelected.metodoDePagamento === "pix") {
+
+            const pix = await Pix.create({
+                qrCode: "123124124123",
+                vencimento: "31/12/2022"
+            });
+
+            req.session.pixId = pix.id;
+
+        } else {
+
+            const ticket = await Ticket.create({
+                codigoBarras: "12390.00005 00000.00006 00000.000007 8 56760000015075",
+                codigoPdf: "https://www.africau.edu/images/default/sample.pdf",
+                vencimento: "31/12/2022"
+            });
+
+            req.session.ticketId = ticket.id;
+            
+        };
+
+        res.redirect('/compra-confirmada');
+
+    },
+
+    purchaseFinalization: async (req, res) => {
+
+        !req.session.creditCardId || !req.session.pixId || !req.session.ticketId ? res.redirect('/login') : res.render('confirmation');
+
+        await Purchase.create({
+            where: {
+                usuarioId: req.session.user.id,
+                freteId: req.session.shippingId,
+                enderecoId: req.session.addressId,
+                boletoId: req.session.ticketId,
+                pixId: req.session.pixId,
+                cartaoId: req.session.creditCardId,                
+                status: "Compra finalizada com sucesso",
+                numeroParcela: req.session.parcelNumbers
+            }
+        })
+
     }
 }
 
