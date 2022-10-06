e406a1e482dc2833a64d0a4bb0b4c40798b5db38
Andressa Paranhos <andressaparanhos1@hotmail.com>
2022-10-06 11:54:15 -0300

    add: novo controller puxando os produtos do banco

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -4,27 +4,26 @@
 const { Product } = require('../models');
 const { createMenuObject } = require('../../helpers/createMenuObject');
 
+
 const ProductController = {
     // Função responsável por buscar no banco de dados o produto por sua categoria
     findByCategory: async (req, res) => {
         // Busca na requisição o parametro categoria que foi inserido na url
-        const { category } = req.params;
+        const categoria = req.params.categoria;
         // Busca no banco de dados todos os produtos existente
         const products = await Product.findAll({ 
-        // Where: Filtra e exibi somente os produtos que possuem a mesma categoria passada na url
-            where: { 
-                categoria: category
-            }
+           // Where: Filtra e exibi somente os produtos que possuem a mesma categoria passada na url
+        })
+        console.log(JSON.stringify(products))
+        res.render('department', {
+            menu: createMenuObject(categoria),
+            banner: {
+                background: `departamento_${categoria}.jpg`
+            },
+            products, categoria
         });
-        return res.render('department',{ menu: createMenuObject(categoria), 
-            banner: {background: `departamento_${categoria}.jpg`}})
-        // Renderiza a view department
-    },
-
+    }
 }
-
-
-
 
 
 module.exports = ProductController;
